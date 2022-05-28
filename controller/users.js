1b2149103cbd33194912a5cd39a82d673ac8a734
ernesttsai0713 <deviltasi@gmail.com>
2022-05-28 20:24:59 +0800

    f/user responseHandle

--- controller/users.js
+++ controller/users.js
@@ -1,35 +1,26 @@
 const Users = require('../models/users');
-const Res = require('../responseHandle');
+const { hasSuccess, hasError  } = require('../responseHandle');
 
 const users = {
     getUserInfo: async (req, res, next) => {
-        try{
-            const data = await Users.find();
-        }catch(e){
-            console.log(e);
-        }
+        const data = await Users.find();
+        hasSuccess(res, 200, data)
     },
     createAccount: async (req, res, next)=>{
-        try {
-            const data = req.body;
+        const data = req.body;
+        const filter = {
+            email:data.email
+        }
+        // console.log(req.body);
+        const checkEmailOnly = await Users.find(filter);
+        // console.log(checkEmailOnly)
+        if( checkEmailOnly ){
+            const errMsg = '帳號已存在'
+            return next(hasError(400, errMsg, next))
+        } else {
             const msg = '帳號創建成功'
-            const filter = {
-                email:data.email
-            }
-            // console.log(req.body);
-            const checkEmailOnly = await Users.find(filter);
-            // console.log(checkEmailOnly)
-            if( checkEmailOnly ){
-                // console.log(checkEmailOnly);
-                Res.error(res, 400,'帳號已存在')
-            } else {
-                const newUser = await Users.create(data);
-                Res.success(res, 200, msg, newUser);
-            }
-        } catch(e) {
-            console.log(e.message);
-            const msg = '帳號格式不正確';
-            Res.error(res,400,msg);
+            const newUser = await Users.create(data);
+            hasSuccess(res, 200, { msg, newUser});
         }
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const users = require('../controller/users');
+const { handleErrorAsync } = require('../responseHandle');
 
 /* GET users listing. */
-router.post('/', users.createAccount);
-router.post('/perfile', users.getUserInfo);
+router.post('/', handleErrorAsync(users.createAccount));
+router.post('/perfile', handleErrorAsync(users.getUserInfo));
 
 module.exports = router;
